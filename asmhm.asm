799a3f5a4859e6889eb14339e0e563ac0e50c442
segward <gustavhaverstad@gmail.com>
2025-10-04 01:53:27 +0200

    write lines

--- asmhm.asm
+++ asmhm.asm
@@ -15,48 +15,79 @@ section .text
   extern _fread
   extern _fclose
 
-; arg1 string
-; arg2 length
-; arg3 lines array
+; arg1: string
+; arg2: length
+; arg3: lines array
 ; return: number of lines
 _split_lines:
+  ; rax: line count 
+  ; r8: start of current line
+  ; r9: line index
   xor rax, rax
   mov r8, rdi
   xor r9, r9 
 
 .loop:
+  ; reached end of buffer
   cmp rsi, 0
   je .done
 
   ; check for newline
-  mov bl, [rdi]
-  cmp bl, 10
-  jne .next_byte
+  cmp byte [rdi], 10
+  jne .next
 
-  ; found newline
-  mov byte [rdi], 0
+  ; store line start in lines array
   mov [rdx + r9*8], r8
   inc rax
   inc r9
-  inc rdi
-  dec rsi
   mov r8, rdi
-  jmp .loop
 
-.next_byte:
+.next:
+  ; move to next character
   inc rdi
   dec rsi
   jmp .loop
 
 .done:
-  ; handle last line if not empty
+  ; store last line if not empty
   cmp rdi, r8
   je .end
 
+  ; store line start in lines array
   mov [rdx + r9*8], r8
   inc rax
 
 .end:
+  ret
+
+; arg1: lines array
+; arg2: number of lines
+; return: void
+_write_lines:
+  ; r8: line index
+  xor r8, r8
+  
+.loop:
+  ; reached end of lines
+  cmp r8, rsi
+  jge .done
+
+  ; load line to rcx
+  mov rcx, [rdi + r8*8]
+
+  ; print structure
+  ; arg1: format
+  ; arg2: line
+  lea rdi, [rel fmt]
+  mov rsi, rcx
+  xor rax, rax
+  call _printf
+
+  ; move to next line
+  inc r8
+  jmp .loop
+
+.done:
   ret
 
 _main:
@@ -96,8 +127,15 @@ _main:
   lea rdx, [rel lines]
   call _split_lines
 
-  ; store number of lines in r14
-  mov r14, rax
+  ; store number of lines in r13
+  mov r13, rax
+
+  ; write lines
+  ; arg1: lines array
+  ; arg2: number of lines
+  lea rdi, [rel lines]
+  mov rsi, r13
+  call _write_lines
 
   ; fclose structure
   ; arg1: FILE*
@@ -106,5 +144,5 @@ _main:
   call _fclose
 
   ; exit program 
-  mov rdi, r14
+  mov rdi, r13
   call _exit
